31cb6eab60d63ccccfbf9753a105602d6b33afb6
maritvandijk <mlvandijk@gmail.com>
2018-04-08 08:16:23 +0200

    Updated for additional primitive types

--- src/test/java/expressions/StepDefs.java
+++ src/test/java/expressions/StepDefs.java
@@ -7,55 +7,86 @@ import cucumber.api.java.en.When;
 
 public class StepDefs {
 
+    // Question: how does it "know: to generate expressions not regex? Could I still generate regex if I prefer?
+
+    // IntelliJ plugin:
+    // - IntelliJ gives an error on {int} or {double} or other number type saying "Number expected"
+    // - IntelliJ also gives an error on {word} or {string} saying "Number expected"
+    // - Steps using cucumber expressions are not recognized by IntelliJ
+
     int cucumbers;
-    double cucumberSlices;
+    long longCucumbers;
+    double doubleSlices;
     float floatingSlices;
 
-    // + Generated snippet contains cucumber expression {int}
-    // - IntelliJ gives an error on {int} saying "Number expected"
-    // Question: how does it "know: to generate expressions not regex? Could I still generate regex if I prefer?
-//    @Given("I have {int} cucumbers in my belly")
-//    public void i_have_cucumbers_expressions_in_my_belly(int integer) {
-//        cucumbers = integer;
+    // 1. Using numbers that are integers:
+    // Can use only one integer type, otherwise AmbiguousStepDefinitionsException is thrown (as expected)
+
+//    // + Generated snippet contains cucumber expression {int}
+    @Given("I have {int} cucumber(s) in my belly/stomach")
+    public void i_have_cucumbers_expressions_in_my_belly(int number) {
+        cucumbers = number;
+    }
+
+    // - Using {byte} doesn't work (according to the docs it should)
+    // Expected result: Step definition is found
+    // Actual result: Cucumber suggests snippets with an {int}
+//    @Given("I have {byte} cucumber(s) in my belly/stomach")
+//    public void i_have_byte_cucumbers_expressions_in_my_belly(byte number) {
+//        cucumbers = number;
+//    }
+
+    // + Using {short} works!
+//    @Given("I have {short} cucumber(s) in my belly/stomach")
+//    public void i_have_short_cucumbers_expressions_in_my_belly(short number) {
+//        cucumbers = number;
+//    }
+
+    // + Using {long} works (although only if variable is also a long - as expected in Java)!
+//    @Given("I have {long} cucumber(s) in my belly/stomach")
+//    public void i_have_long_cucumbers_expressions_in_my_belly(long number) {
+//        longCucumbers = number;
 //    }
 
     // + Nice; I can replace it with a regex step and use regex combined with expressions!
+    // Also still works with cucumbers? to match cucumber/cucumbers
+    // I do have to match belly and stomach separately, which is another improvement in cucumbers expressions!
     // (My question was: Can I still use regex? (and how?) Or will all my existing tests break?)
-    // Note that steps using cucumber expressions are not recognized by IntelliJ; the plugin will need to be updated.
-    @Given("^I have (\\d+) cucumbers in my belly$")
-    public void i_have_cucumbers_in_my_belly(int integer) {
-        cucumbers = integer;
-    }
-
-    // Generated snippet with double has two arguments, both ints (I'm assuming it's the numbers before and after the decimal)
-    // - Running it leads to cucumber.runtime.CucumberException: Arity mismatch: Step Definition 'expressions.StepDefs.i_have_cucumbers_in_my_belly(int,int)
-//    @Given("I have {double} cucumbers in my belly")
-//    public void i_have_cucumbers_in_my_belly(int arg1, int arg2) {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new PendingException();
+    // Also I noticed that {int} does match negative numbers, while (\\d+) does not.
+//    @Given("^I have (\\d+) cucumbers? in my belly$")
+//    public void i_have_cucumbers_in_my_belly(int integer) {
+//        cucumbers = integer;
+//    }
+//
+//    @Given("^I have (\\d+) cucumbers? in my stomach")
+//    public void i_have_cucumbers_in_my_stomach(int integer) {
+//        cucumbers = integer;
 //    }
 
-    // Workaround: Changing argument to double works (but generated snippet really should be correct)
+    // 2. Using decimal numbers
+
     // Question: documentation gives example using float, but double is generated. This should be consistent?
-//    @Given("I have {double} cucumbers in my belly")
-//    public void i_have_cucumbers_in_my_belly(double slices) {
-//        cucumberSlices = slices;
-//    }
+    @Given("I have {double} cucumbers in my belly")
+    public void i_have_cucumbers_in_my_belly(double double1) {
+        doubleSlices = double1;
+    }
 
     // Having both a step def with double and float gives cucumber.runtime.AmbiguousStepDefinitionsException:
     // "I have 42.5 cucumbers in my belly" matches more than one step definition:
     // (Which is as expected :)
+
     // Using float instead of double works.
 //    @Given("I have {float} cucumbers in my belly")
 //    public void i_have_cucumbers_in_my_belly(float slices) {
 //        floatingSlices = slices;
 //    }
 
-    @Given("I have {double} cucumbers in my belly")
-    public void i_have_cucumbers_in_my_belly(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+
+
+//    @Given("I have {float} cucumbers in my belly")
+//    public void i_have_cucumbers_in_my_belly(float float1) {
+//        floatingSlices = float1;
+//    }
 
     // Obviously it doesn't generate a step with {word} in it, because how would it know which word is my variable
     // Replacing with {word} AND adding an argument works
--- src/test/resources/expressions/parameter-types.feature
+++ src/test/resources/expressions/parameter-types.feature
@@ -1,8 +1,24 @@
 Feature: Check that primitives work in cucumber expressions
 
-  Scenario: Using numbers
+  Scenario: Using numbers int, double, float
     Given I have 42 cucumbers in my belly
+    And I have 1 cucumber in my stomach
     And I have 42.5 cucumbers in my belly
     When I test expressions
     And I test cucumbers
     Then my test should "very surely pass"
+
+  Scenario: Using bigint, bigdec
+
+  Scenario: Using byte value of -128 and a maximum value of 127 (inclusive)
+    Given I have 127 cucumbers in my belly
+    And I have -128 cucumbers in my stomach
+    When I test expressions
+    And I test cucumbers
+    Then my test should "very surely pass"
+
+  Scenario: Using short value of -32,768 and a maximum value of 32,767 (inclusive)
+
+  Scenario: Using long
+
+  Scenario: Using double
